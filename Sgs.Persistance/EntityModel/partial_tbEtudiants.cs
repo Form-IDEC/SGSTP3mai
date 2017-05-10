92421bcdf124dc639ec56a08c363357c90570cd9
jay <jayrdee@gmail.com>
2017-05-10 16:14:12 +0200

    Ajoutez des fichiers projet.

---
+++ Sgs.Persistance/EntityModel/partial_tbEtudiants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sgs.Persistance.EntityModel
+{
+    public partial class tbEtudiants
+    {
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Prenom, Nom);
+        }
+    }
+}
---
+++ Sgs.Presentation/ViewModel/EtudiantListViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Sgs.Business.Model;
+using System.Collections.ObjectModel;
+using Sgs.Persistance.EntityModel;
+
+namespace Sgs.Presentation.ViewModel
+{
+    public class EtudiantListViewModel
+    {
+        private ObservableCollection<tbEtudiants> _etudiants;
+
+        public EtudiantListViewModel()
+        {
+            EntityModelConnexion context = new EntityModelConnexion();
+            _etudiants = new ObservableCollection<tbEtudiants>(context.tbEtudiants);
+
+            //foreach (tbEtudiants etudiant in context.tbEtudiants.ToList<tbEtudiants>())
+            //    _etudiants.Add(etudiant);
+                       
+        
+           
+          
+        }
+
+        public ObservableCollection<tbEtudiants> Etudiants
+        {
+            get { return _etudiants; }
+            set
+            {
+                _etudiants.Clear();
+                _etudiants = value;
+            }
+        }
+
+      
+
+
+    }
+}
